d85e2e2773848bf10a772ffa4365c46e08124b8e
bearl <125728897+bearl27@users.noreply.github.com>
2024-06-08 18:54:44 +0900

    初期段階

---
+++ binarygame2/app/src/main/java/jp/ac/ritsumei/ise/phy/exp2/is0682hi/binarygame2/GameActivity.java
@@ -0,0 +1,16 @@
+package jp.ac.ritsumei.ise.phy.exp2.is0682hi.binarygame2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+
+}
